1a070488c0c4bd36eaf370c35553159999ba9168
FilUnderscore <fil@filunderscore.com>
2024-09-29 03:52:03 +1300

    Add loss recorder.

--- fl_sample_model.py
+++ fl_sample_model.py
@@ -2,11 +2,26 @@ from torch.nn import L1Loss
 from darts.models import RNNModel
 from darts.utils.callbacks import TFMProgressBar
 
+from darts import TimeSeries
+from pytorch_lightning.callbacks import Callback
+import matplotlib.pyplot as plt
+
+class LossRecorder(Callback):
+    def __init__(self):
+        self.train_loss_history = []
+        self.val_loss_history = []
+
+    def on_train_epoch_end(self, trainer, pl_module):
+        self.train_loss_history.append(trainer.callback_metrics["train_loss"].item())
+        self.val_loss_history.append(trainer.callback_metrics["val_loss"].item())
+
+loss_recorder = LossRecorder()
+
 def generate_torch_kwargs():
     return {
         "pl_trainer_kwargs": {
             "accelerator": "cpu",
-            "callbacks": [TFMProgressBar(enable_train_bar_only=False)]
+            "callbacks": [TFMProgressBar(enable_train_bar_only=False), loss_recorder]
         }
     }
 
--- single_peer.py
+++ single_peer.py
@@ -6,7 +6,7 @@ import datetime
 
 import matplotlib.pyplot as plt
 
-from fl_sample_model import model
+from fl_sample_model import model, loss_recorder
 
 def strToDateTime(str):
     return datetime.datetime.strptime(str, '%d/%m/%Y')
@@ -25,7 +25,18 @@ peer.train()
 
 prediction = peer.ml_model.predict(365)
 
+plt.figure(0)
 csv_ts_data_provider.get_data().plot()
 prediction.plot(label="forecast", low_quantile=0.05, high_quantile=0.95)
 plt.legend()
+plt.title(f'Training Results')
+
+plt.figure(1)
+plt.plot(loss_recorder.train_loss_history, label='Train Loss')
+plt.plot(loss_recorder.val_loss_history, label='Val Loss')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.legend()
+plt.title(f'Accuracy')
+
 plt.show()
